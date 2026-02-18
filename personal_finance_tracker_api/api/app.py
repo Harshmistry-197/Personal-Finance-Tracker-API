4811dbfe63b6542447ada65e31aba4732c887920
Harshmistry-197 <harshmistry072004@gmail.com>
2026-02-18 14:13:35 +0530

    Change : Remove class model from this file and created seprate file

--- personal_finance_tracker_api/api/app.py
+++ personal_finance_tracker_api/api/app.py
@@ -1,40 +1,15 @@
-from typing import Literal, List, Annotated, Optional
-from pydantic import BaseModel, StrictStr, StrictFloat,Field, StringConstraints, AfterValidator
+from typing import Optional
 from fastapi import FastAPI, HTTPException, Body, status, Query
-from datetime import datetime, timezone
 from personal_finance_tracker_api.database import create_transaction, create_category
 from bson import ObjectId
 from pymongo.errors import DuplicateKeyError
+from .schemas import Transaction, Category
 
 
 app = FastAPI()
 
 transaction_col = create_transaction()
 category_col = create_category()
-
-def validate_past_date(v: datetime) -> datetime:
-    if v.tzinfo is None:
-        v = v.replace(tzinfo=timezone.utc)
-    if v > datetime.now(timezone.utc):
-        raise ValueError("Date cannot be in the future")
-    return v
-
-class Transaction(BaseModel):
-     title: Annotated[str, StringConstraints(strip_whitespace=True, min_length=3, max_length=100)]
-     description: StrictStr = Field(..., min_length=1)
-     amount: StrictFloat = Field(..., gt=0)
-     type: Literal["income", "expense"]
-     category: Annotated[str, StringConstraints(to_lower=True, min_length=1, strip_whitespace=True)]
-     date: Annotated[datetime, AfterValidator(validate_past_date)]
-     tags: List[Annotated[str, StringConstraints(max_length=30)]] = Field(..., max_length=10)
-     created_at: datetime
-     updated_at: datetime
-
-class Category(BaseModel):
-    name: StrictStr = Field(..., min_length=1, max_length=100)
-    type: Literal["income", "expense", "both"]
-    description: StrictStr = Field(..., min_length=1)
-    created_at: datetime
 
 
 # Transaction API

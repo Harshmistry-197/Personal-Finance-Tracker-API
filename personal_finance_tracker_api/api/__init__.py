5bef331f2bbd417f5624e6f0ac42b7b2121f7a1e
Harshmistry-197 <harshmistry072004@gmail.com>
2026-02-18 14:12:28 +0530

    Change : Chenged the init file to include function name

--- personal_finance_tracker_api/api/__init__.py
+++ personal_finance_tracker_api/api/__init__.py
@@ -1,3 +1,4 @@
 from personal_finance_tracker_api.api import app
+from personal_finance_tracker_api.api.schemas import Transaction, Category
 
-__all__ = ['app']
+__all__ = ['app', 'Transaction', 'Category']

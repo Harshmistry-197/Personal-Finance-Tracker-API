0347cd632462c6336254f259e3d894c6ad50d8d6
Harshmistry-197 <harshmistry072004@gmail.com>
2026-02-18 14:37:31 +0530

    Add : Added the docstring and comments wherever required

--- personal_finance_tracker_api/database/data.py
+++ personal_finance_tracker_api/database/data.py
@@ -3,11 +3,20 @@ import os
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
+# Load environment variables from .env file
 load_dotenv()
+
+# Retrieve MongoDB connection string from environment variables
 connection = os.getenv("URL")
 
 
 def create_transaction():
+    """
+        Initializes a connection to the 'transaction' collection in MongoDB.
+        - Uses AsyncIOMotorClient for non-blocking database operations.
+        - Connects to the 'Finance' database.
+        - Returns: Motor Collection object for transactions.
+    """
     try:
         client = AsyncIOMotorClient(connection)
         print("Client Created")
@@ -24,6 +33,11 @@ def create_transaction():
 
 
 def create_category():
+    """
+        Initializes a connection to the 'category' collection in MongoDB.
+        - Uses AsyncIOMotorClient to maintain consistency with FastAPI's async nature.
+        - Returns: Motor Collection object for categories.
+    """
     try:
         client = AsyncIOMotorClient(connection)
         print("Client Created")
